aecb381bda59f0742b47c9babc6530f073cc2ef9
Gixzilla <gladiator.girish@gmail.com>
2026-04-28 17:35:34 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,29 @@
-1
+variable "buckets" {
+  type = map(object({
+    bucket_name = string
+    environment = string
+  }))
+}
+
+locals {
+test_metadata = {
+api_key = "XVGYUhyauety23899ajjjagGGGG"
+username = "demo"
+}
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  for_each = var.buckets
+
+  bucket = each.value.bucket_name
+  
+  tags = {
+    Name        = each.value.bucket_name
+    Environment = each.value.environment
+    test        = "hello"
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
